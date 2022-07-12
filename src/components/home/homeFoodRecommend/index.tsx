b459a19b05335c66ab89a7cd43eafa3fe226ca09
ldh3907 <ldh165163@gmail.com>
2022-07-12 23:49:00 +0900

    ⚡️ ::홈 음식 추천 컴포넌트 수정

--- src/components/home/homeFoodRecommend/index.tsx
+++ src/components/home/homeFoodRecommend/index.tsx
@@ -1,7 +1,23 @@
-import { HomeFoodRecommentContainer } from "./style";
+import {
+  HomeFoodRecommendContainer,
+  HomeFoodRecommendFoodDescription,
+  HomeFoodRecommendFoodIcon,
+  HomeFoodRecommendFoodName,
+  HomeFoodRecommendTitle,
+} from "./style";
 
 const HomeFoodRecommend = () => {
-  return <HomeFoodRecommentContainer></HomeFoodRecommentContainer>;
+  return (
+    <HomeFoodRecommendContainer>
+      <HomeFoodRecommendTitle>먹어도 괜찮을까?</HomeFoodRecommendTitle>
+      <HomeFoodRecommendFoodIcon />
+      <HomeFoodRecommendFoodName>치즈</HomeFoodRecommendFoodName>
+      <HomeFoodRecommendFoodDescription>
+        염분과 유당 성분이 매우 적은 제품을 먹이거나 강아지 전용으로 출시된
+        치즈를 구입해서 먹여야 합니다.
+      </HomeFoodRecommendFoodDescription>
+    </HomeFoodRecommendContainer>
+  );
 };
 
 export default HomeFoodRecommend;
